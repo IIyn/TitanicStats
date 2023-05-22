519edda9667a070f7436522b984bffe5f67378b4
yassin <yassinam.pro@gmail.com>
2023-05-22 15:53:12 +0200

    :memo: [NEW] added models for users and trying to add neew route for users

--- server/src/server.js
+++ server/src/server.js
@@ -1,18 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 import hello from "./routes/hello.js";
+import users from "./routes/users.js";
 
 const app = express();
 
 dotenv.config("../.env");
-const { APP_URL, APP_PORT } = process.env;
+const { APP_URL, APP_PORT, MONGO_URL } = process.env;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.use("/hello", hello);
+app.use("/users", users);
 
-app.listen(APP_PORT, () => {
-  console.log(`Server running on http://${APP_URL}:${APP_PORT}`);
-});
+try {
+  await mongoose.connect(MONGO_URL);
+  console.log("Connexion MonboDB établie!");
+
+  app.listen(APP_PORT, () =>
+    console.log(`L'application écoute sur http://${APP_URL}:${APP_PORT}`)
+  );
+} catch (err) {
+  console.log("Impossible de démarrer l'application Node", err.message);
+}
