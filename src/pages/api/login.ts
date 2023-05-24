1971881115f7745dcb6c8e679f66a99cd731649d
yassin <yassinam.pro@gmail.com>
2023-05-24 16:52:45 +0200

    :memo: [FIX] added try catch to token signing to fix deployed app error

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -24,11 +24,14 @@ export default async function handler(request: any, response: any) {
         .json({ message: "Mot de passe ou email invalide" });
     }
 
-    const token = jwt.sign({ name: user.name }, process.env.JWT_SECRET_KEY, {
-      expiresIn: "1h",
-    });
-
-    response.status(200).json({ token });
+    try {
+      const token = jwt.sign({ name: user.name }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "1h",
+      });
+      response.status(200).json({ token });
+    } catch (err) {
+      response.status(401).json({ message: "Invalid token signing" });
+    }
   } catch (err) {
     console.error("Une erreur s'est produite :", err);
     response.status(500).json({ message: "Une erreur s'est produite" });
